0c11f662578f149ed393e0fc63d5c706c17574b5
bgutsol <bogdan.gutsol@gmail.com>
2018-08-29 23:38:43 +0300

    fix tiles dragging bug

--- src/components/TilesList/TilesList.vue
+++ src/components/TilesList/TilesList.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   .tiles-list
-    Draggable.tiles-list__list(element="ul" v-model="list" :options="listOptions")
+    Draggable.tiles-list__list(element="ul" v-model="list" :options="listOptions" @start="onDraggingStart" @end="onDraggingEnd")
       li.tiles-list__item(v-for="item in list" :key="item.id")
         Tile(:item="item")
     button.tiles-list__btn-add(@click="addTile") + Add tile to trading desk
@@ -36,6 +36,16 @@ export default {
       addTile: 'addListItem',
       updateList: 'updateList',
     }),
+    onDraggingStart({ item }) {
+      const resizeDetector = item.querySelector('object');
+
+      resizeDetector.style.display = 'none';
+    },
+    onDraggingEnd({ item }) {
+      const resizeDetector = item.querySelector('object');
+
+      resizeDetector.style.display = 'block';
+    },
   },
   components: {
     Draggable,
